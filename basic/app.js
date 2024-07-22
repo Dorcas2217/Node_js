aa7d9fcabf9aaf33268a5c43a9a67283e23e13b2
dorcasMichelle <dorcas.kuezenounga@student.vinci.be>
2024-07-22 13:33:11 +0200

    part 1

--- basic/app.js
+++ basic/app.js
@@ -6,15 +6,26 @@ var logger = require('morgan');
 var pizzaRouter = require('./routes/pizzas');
 var usersRouter = require('./routes/users');
 
-var app = express();
 
+var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', pizzaRouter);
+var counter = 0
+app.use((req,res,next) => {
+    counter += 1
+    if(req.url == 'GET'){
+        console.log("GET Counter : ", counter);
+    }
+    else if(req.url == '/pizzas'){
+        console.log("GET Counter : ", counter);
+    }
+    next();
+})
+app.use('/pizzas', pizzaRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
--- basic/routes/pizzas.js
+++ basic/routes/pizzas.js
@@ -29,10 +29,130 @@ const MENU = [
   },
 ];
 
-// Read all the pizzas from the menu
+/* Read all the pizzas from the menu
+   GET /pizzas?order=title : ascending order by title
+   GET /pizzas?order=-title : descending order by title
+*/
 router.get('/', (req, res, next) => {
-  console.log('GET /pizzas');
-  res.json(MENU);
+  const order = req?.query?.order?.includes('title')? req.query.order : undefined
+
+  let menuOrder
+  if(order) 
+    menuOrder = [...MENU].sort((a,b) => a.title.localeCompare(b.title));
+
+  if(order == '-title')
+    menuOrder.reverse();
+  
+  res.json( menuOrder ?? MENU);
 });
+
+router.get('/:id', (req, res, next) => {
+  const id = req?.params?.id? parseInt(req.params.id): undefined;
+  if(!id)
+    return res.sendStatus(400)
+
+  const pizzas = MENU.find(p => p.id == id)
+  if(!pizzas)
+    return res.sendStatus(404)
+
+  res.json(pizzas);
+  
+});
+
+
+router.post('/', (req, res) => {
+  const title = req?.body?.title?.trim().length ? req.body.title : undefined;
+  const content = req?.body?.content?.trim().length ? req.body.content : undefined;
+
+  console.log('POST /pizzas');
+
+  if (!title || !content) return res.sendStatus(400); // error code '400 Bad request'
+
+  const lastItemIndex = MENU.length ? MENU.length - 1 : undefined;
+  const lastId = lastItemIndex !== undefined ? MENU[lastItemIndex]?.id : 0;
+  const nextId = lastId + 1;
+
+  const newPizza = {
+    id: nextId,
+    title: title,
+    content: content,
+  };
+
+  MENU.push(newPizza);
+
+  res.json(newPizza);
+});
+
+// Delete a pizza from the menu based on its id
+router.delete('/:id', (req, res) => {
+  const id = req.params.id? parseInt(req.params.id, 10) : undefined;
+
+  if(!id ) 
+    return res.sendStatus(400);
+
+  const foundIndex = MENU.findIndex(pizza => pizza.id == id);
+
+  if (foundIndex < 0)
+    return res.sendStatus(404);
+
+  const itemsRemovedFromMenu = MENU.splice(foundIndex, 1);
+  const itemRemoved = itemsRemovedFromMenu[0];
+
+  res.json(itemRemoved);
+});
+
+// Update or create a pizza based on its id and new values for its parameters
+router.put('/:id', (req, res) => {
+  const id = req.params.id? parseInt(req.params.id, 10) : undefined;
+
+  if (!id || id < 0) return res.status(400).send('BAD Request : A correct ID is required');
+
+  const title = req?.body?.title?.trim().length ? req.body.title : undefined;
+  const content = req?.body?.content?.trim().length ? req.body.content : undefined;
+
+  if (!title || !content) return res.send(400).send('all the property of the request must be fill correctly (title and content)');
+
+  const foundIndex = MENU.findIndex(pizza => pizza.id == id);
+
+  if (foundIndex < 0) {
+    const newPizza = {
+      id: id,
+      title: title,
+      content: content,
+    };
+  
+    MENU.push(newPizza);
+    return res.json(newPizza);
+  }
+
+  const updatedPizza = {...MENU[foundIndex], ...req.body};
+
+  MENU[foundIndex] = updatedPizza;
+
+  res.json(updatedPizza);
+});
+
+// Update a pizza based on its id and new values for its parameters
+router.patch('/:id', (req, res) => {
+  const id = req.params.id? parseInt(req.params.id, 10) : undefined;
+
+  if (!id) return res.status(400).send('BAD Request : ID is required');
+
+  const title = req?.body?.title;
+  const content = req?.body?.content;
+
+  if ((!title && !content) || title?.length === 0 || content?.length === 0) return res.sendStatus(400);
+
+  const foundIndex = MENU.findIndex(pizza => pizza.id == req.params.id);
+
+  if (foundIndex < 0) return res.sendStatus(404);
+
+  const updatedPizza = {...MENU[foundIndex], ...req.body};
+
+  MENU[foundIndex] = updatedPizza;
+
+  res.json(updatedPizza);
+});
+
 
 module.exports = router;
